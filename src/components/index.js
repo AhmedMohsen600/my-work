cfb8197f52b667ffc65e58871ef707c5fd842d8a
AhmedMohsen600 <ahmedmohsen159357@gmail.com>
2021-12-01 21:58:03 +0200

    remove warnings

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,3 @@
 export { default as Header } from "./header";
 export { default as Hero } from "./hero";
-export { default as CustomizedAccordions } from "./accordion";
 export { default as MyWork } from "./my-work";
--- src/containers/accordians.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import { CustomizedAccordions } from "../components";
-export function AccordiansContainer() {
-  return <CustomizedAccordions />;
-}

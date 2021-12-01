a0dc518c1e65577d80eb2412fb64af0e2ee5afe1
AhmedMohsen600 <ahmedmohsen159357@gmail.com>
2021-12-01 22:09:02 +0200

    fixed the error

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { render } from "react-dom";
 import { GlobalStyles } from "./global-styles";
 import "normalize.css";
-import App from "./app";
+import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 render(
